2f4493f8ae2edf918d04f8bad5b0fb417c92e3d8
Jaszy <jaszydh@gmail.com>
2025-03-07 02:57:56 +0100

    blocks flipcard

--- components/block_sportplan.php
+++ components/block_sportplan.php
@@ -1,19 +1,34 @@
 <?php
-function renderBlock($header, $content, $image)
+function renderBlock($header, $content, $image, $frontContent)
 {
 ?>
-    <div class="block">
-        <div class="block-header">Fitplan: <?php echo htmlspecialchars($header); ?></div>
-        <div class="block-content">
-            <ul class="content-list">
-                <?php foreach ($content as $item): ?>
-                    <li>
-                        <img src="assets/img/dumbell.png" alt="Bullet Point" class="list-icon">
-                        <span class="list-text"><?php echo htmlspecialchars($item); ?></span>
-                    </li>
-                <?php endforeach; ?>
-            </ul>
-            <img src="<?php echo htmlspecialchars($image); ?>" alt="Block Image" class="content-image">
+    <div class="block" onclick="flipCard(this)">
+        <div class="block-inner">
+            <!-- Voorkant met dynamische inhoud -->
+            <div class="block-front">
+                <div class="block-content front-content">
+                    <ul>
+                        <?php foreach ($frontContent as $item): ?>
+                            <li class="front-text"><?php echo htmlspecialchars($item); ?></li>
+                        <?php endforeach; ?>
+                    </ul>
+                </div>
+            </div>
+
+            <!-- Achterkant: Behoudt originele styling -->
+            <div class="block-back">
+                <div class="block-header">Fitplan: <?php echo htmlspecialchars($header); ?></div>
+                <div class="block-content">
+                    <ul class="content-list">
+                        <?php foreach ($content as $item): ?>
+                            <li>
+                                <span class="list-text"><?php echo htmlspecialchars($item); ?></span>
+                            </li>
+                        <?php endforeach; ?>
+                    </ul>
+                    <img src="<?php echo htmlspecialchars($image); ?>" alt="Block Image" class="content-image">
+                </div>
+            </div>
         </div>
     </div>
 <?php
--- sportplan.php
+++ sportplan.php
@@ -27,60 +27,57 @@ include_once 'components/block_sportplan.php'; // Verwijzing aangepast
     <div class="block-container">
         <?php
         $blocks = [
-            ["header" => "Medium Cardio", "content" => [
-                "Voedingsschema's op maat",
-                "3 cardiotrainingen per week van 1,5 uur onder begeleiding",
-                "Extra stretching en warming-up schema's"
-            ], "image" => "assets/img/cardio-middle.png"],
-
-            ["header" => "Senior Fit", "content" => [
-                "Focus op balans, flexibiliteit en kracht",
-                "Gezonde maaltijden & voedingsadvies",
-                "2x per week senior-fitness onder begeleiding",
-            ], "image" => "assets/img/senior.png"],
-
-            ["header" => "Weight fit Medium", "content" => [
-                "1x per week personal trainer",
-                "Voedingsadvies voor spieropbouw",
-                "3x per week gewichtstraining met schema",
-            ], "image" => "assets/img/experienced.png"],
-
-            ["header" => "Cardio Extra", "content" => [
-                "Extra cardio en HIIT-schema's",
-                "5x per week intensieve trainingen",
-                "Incl. weeg- en meetmomenten"
-            ], "image" => "assets/img/extra-cardio.png"],
-
-            ["header" => "Complete Package", "content" => [
-                "Alles-in-één: cardio, krachttraining & voeding",
-                "4x per week personal training",
-                "Toegang tot exclusieve fitnessgroepen"
-            ], "image" => "assets/img/full-package.png"],
-
-            ["header" => "Hardcore", "content" => [
-                "Trainingsprogramma voor gevorderden",
-                "5 dagen per week krachttraining",
-                "2x per week personal trainer"
-            ], "image" => "assets/img/extra-gym.png"],
-
-            ["header" => "Gaining Weight", "content" => [
-                "Bulkdieet en voedingsadvies",
-                "5 dagen per week krachttraining",
-                "1-op-1 begeleiding bij spieropbouw"
-            ], "image" => "assets/img/gaining.png"]
+            [
+                "header" => "Medium Cardio",
+                "frontContent" => ["Milde cardio"],
+                "content" => ["Voedingsschema's op maat", "3 cardiotrainingen per week van 1,5 uur onder begeleiding", "Extra stretching en warming-up schema's"],
+                "image" => "assets/img/cardio-middle.png"
+            ],
+            [
+                "header" => "Senior Fit",
+                "frontContent" => ["Senior Fit"],
+                "content" => ["Focus op balans, flexibiliteit en kracht", "Gezonde maaltijden & voedingsadvies", "2x per week senior-fitness onder begeleiding"],
+                "image" => "assets/img/senior.png"
+            ],
+            [
+                "header" => "Weight Fit Medium",
+                "frontContent" => ["Weight Fit Medium"],
+                "content" => ["1x per week personal trainer", "Voedingsadvies voor spieropbouw", "3x per week gewichtstraining met schema"],
+                "image" => "assets/img/experienced.png"
+            ],
+            [
+                "header" => "Cardio Extra",
+                "frontContent" => ["Cardio Extra"],
+                "content" => ["Extra cardio en HIIT-schema's", "5x per week intensieve trainingen", "Incl. weeg- en meetmomenten"],
+                "image" => "assets/img/extra-cardio.png"
+            ],
+            [
+                "header" => "Complete Package",
+                "frontContent" => ["Complete fitnessplan"],
+                "content" => ["Alles-in-één: cardio, krachttraining & voeding", "4x per week personal training", "Toegang tot exclusieve fitnessgroepen"],
+                "image" => "assets/img/full-package.png"
+            ],
+            [
+                "header" => "Hardcore",
+                "frontContent" => ["Extreme krachttraining"],
+                "content" => ["Trainingsprogramma voor gevorderden", "5 dagen per week krachttraining", "2x per week personal trainer"],
+                "image" => "assets/img/extra-gym.png"
+            ],
+            [
+                "header" => "Gaining Weight",
+                "frontContent" => ["Spiermassa opbouwen"],
+                "content" => ["Bulkdieet en voedingsadvies", "5 dagen per week krachttraining", "1-op-1 begeleiding bij spieropbouw"],
+                "image" => "assets/img/gaining.png"
+            ]
         ];
 
         foreach ($blocks as $block):
-            renderBlock($block["header"], $block["content"], $block["image"]);
+            renderBlock($block["header"], $block["content"], $block["image"], $block["frontContent"]);
         endforeach;
         ?>
     </div>
 </main>
 
 <?php include 'footer.php'; ?>
-
 <script src="assets/js/sportplanScript.js"></script>
-
 </body>
-
-</html>
